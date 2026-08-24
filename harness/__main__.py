03b667664a67358418243aa3fa64e2872c1d5d79
Xylo-o <tuadrian1327@gmail.com>
2026-08-24 17:54:05 +0200

    Small test for testing API calls

--- harness/__main__.py
+++ harness/__main__.py
@@ -6,7 +6,16 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
-response = client.responses.create(model="gpt-5.4-mini", input="Write a short python function to read json data")
 
-print(response.output_text)
+response = client.responses.create(model="gpt-5.4-mini", max_output_tokens=200, input="What's the capital of Poland? Answer in one word")
+correct_response = "Warsaw"
+
+GREEN = "\033[32m"
+RED = "\033[31m"
+RESET = "\033[0m"
+
+if (response.output_text.strip().lower() == correct_response.lower()):
+    print(f"{GREEN}PASS{RESET}")
+else:
+    print(f"{RED}FAIL{RESET}")
 
